a23421f404c34cf8ae38ba5d436dac7b228f3463
jimmygranadino <jimmy.granadino@gmail.com>
2020-08-11 01:37:21 -0700

    layout established

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import './App.css';
+import './images/BGalt.jpg'
 
 function App() {
   return (
-    <div>
-
-    </div>
+    <main>
+      <div className="container">
+        <div className="landing">
+          <div className="title-card">
+            <p className="fullname">JIMMY GRANADINO</p>
+            <p className="role">// Full Stack Developer</p>
+          </div>
+        </div>
+        <div className="content">
+          <div>
+            <h1 className="headings">ABOUT:</h1>
+            <p className="about">I'm an enthuastic solutions-driven developer with an eye towards implementations that optimize decision making. As an analyst by experience & creative at heart pushing me forward, I deconstruct challenges to their base components to find efficient outcomes while utilizing my strength to bring people together for cross-departmental collaboration.</p>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
 
