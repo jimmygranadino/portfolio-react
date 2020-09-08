c056fadf835073ffe0631a06a2f45fec302fe314
jimmygranadino <jimmy.granadino@gmail.com>
2020-09-08 11:03:41 -0700

    about adj

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
     return (
         <div>
             <h1 className="headings">ABOUT:</h1>
-            <p className="content-text">I'm an enthusiastic solutions-driven developer with an eye towards implementations that optimize business intelligence functions. As an analyst by experience & creative at heart pushing me forward, I deconstruct challenges to their base components to find efficient outcomes while utilizing my strength to bring people together for cross-departmental collaboration.</p>
+            <p className="content-text">I'm an enthusiastic solutions-driven developer with an eye towards implementations that optimize business functions. As an analyst by experience & creative at heart pushing me forward, I deconstruct challenges to their base components to find efficient outcomes while utilizing my strength to bring people together.</p>
         </div>
     )
 }
