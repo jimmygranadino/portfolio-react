3ffaa203a219f818445b25018d03f31532a874b8
jimmygranadino <jimmy.granadino@gmail.com>
2020-10-26 23:14:19 -0700

    rebuild complete

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,6 +7,9 @@ const About = () => {
             <div className="content">
                 <h1 className="headings" id="about">ABOUT:</h1>
                 <p className="content-text">I'm an enthusiastic solutions-driven developer with an eye towards implementations that optimize business functions. As an analyst by experience & a creative at heart pushing me forward, I deconstruct challenges to their base components to find efficient outcomes while utilizing my soft skills to bring teams together.</p>
+                <p></p>
+                <p className="content-text">On a personal note - I grew up in Los Angeles, attended university in scenic Santa Cruz, poured it on at a startup in San Francisco, and took a globe-trekking gap year while living in Sydney. My life revolves around sports, music, photography and exploring the world one roadtrip/flight at a time.</p>
+                <p>.</p>
                 <span className="span-border"></span>
             </div>
         </section>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { HashLink as Link } from 'react-router-hash-link';
-import '../images/headerBG.jpg'
 
 const Navbar = () => {
 
@@ -21,11 +20,11 @@ const Navbar = () => {
                 <Link className="navbar-link" to="#portfolio">PORTFOLIO</Link>
                 <Link className="navbar-link" to="#skills">SKILLS</Link> 
                 <Link className="navbar-link" to="#about">ABOUT</Link>
-                <a href="javascript:void(0);" className="navbar-button" onClick={myFunction}>
-                    <svg width="2em" height="2em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <p className="navbar-button" onClick={myFunction}>
+                    <svg width="1.5em" height="1.5em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                         <path fillRule="evenodd" d="M2.5 11.5A.5.5 0 0 1 3 11h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 7h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 3h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
                     </svg>
-                </a>
+                </p>
             </div>
         </nav>
     )
