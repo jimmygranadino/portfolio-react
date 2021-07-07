0a27bd477375deed743d9ea8e992ddb7bfe90bd0
jimmygranadino <jimmy.granadino@gmail.com>
2021-07-06 22:25:17 -0700

    refresh

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Skills from './components/Skills'
 import Contact from './components/Contact'
 import Portfolio from './components/Portfolio'
 import Landing from './components/Landing'
+import Info from './components/Info'
 
 function App() {
   return (
@@ -14,8 +15,7 @@ function App() {
         <div className="container">
         <Router>
           <Landing />
-          <About />
-          <Skills />
+          <Info />
           <Portfolio />
           <div className="landing-bottom">
             <Contact />
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,9 +6,9 @@ const About = () => {
         <section>
             <div className="content">
                 <h1 className="headings" id="about">ABOUT:</h1>
-                <p className="content-text">I'm an enthusiastic solutions-driven developer with an eye towards implementations that optimize business functions. As an analyst by experience & a creative at heart pushing me forward, I deconstruct challenges to their base components to find efficient outcomes while utilizing my soft skills to bring teams together.</p>
+                <p className="content-text">I'm an enthusiastic and analytical developer with an eye towards optimizing business functions. As an analyst by experience, I research challenges and collaborate with cross-functional teams to implement efficient solutions for modern business problems.</p>
                 <p></p>
-                <p className="content-text">On a personal note - I grew up in Los Angeles, attended university in scenic Santa Cruz, poured it on at a startup in San Francisco, and took a globe-trekking gap year while living in Sydney. My life revolves around sports, music, photography and exploring the world one roadtrip/flight at a time.</p>
+                <p className="content-text">On a personal level: I grew up in Los Angeles, attended university in scenic Santa Cruz and have taken a globe-trekking gap year while living in Sydney. My life revolves around sports, music, photography and exploring the world one roadtrip/flight at a time.</p>
                 <p>.</p>
                 <span className="span-border"></span>
             </div>
